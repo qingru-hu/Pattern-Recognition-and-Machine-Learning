7047c9613cc2a38953f0509548e6e45c26108594
Ella <huqr20@mails.tsinghua.edu.cn>
2023-04-10 22:16:50 +0800

    Finish hw6

--- hw6/code/sk_all_algorithms.py
+++ hw6/code/sk_all_algorithms.py
@@ -42,9 +42,6 @@ print("Dataset loading complete.")
 # 这里我们以Decision Tree为例，定义了模型以及其超参数（criterion选什么以及max_depth怎么取），请大家完成其他模型的定义以及超参数设置
 models = {
     "Decision Tree": DecisionTreeClassifier,  # criterion, max_depth
-    ##################################################
-    # TODO: 找到下面每个算法对应的sklearn模型
-    ##################################################
     "Random Forest": RandomForestClassifier, # criterion, n_estimators
     "Bagging": BaggingClassifier, # n_estimators
     "Gradient Boosting": GradientBoostingClassifier, # n_estimators, learning_rate
@@ -57,25 +54,22 @@ models = {
 }
 
 model_hparams_best = {
-    "Decision Tree": dict(criterion='log_loss', max_depth=10),
-    ##################################################
-    # TODO: 为下面每个模型指定超参数
-    ##################################################
-    # "Random Forest":
-    # "Bagging":
-    # "Gradient Boosting":
+    "Decision Tree": dict(criterion='gini', max_depth=5),
+    "Random Forest": dict(n_estimators=50, criterion='entropy'),
+    "Bagging": dict(n_estimators=20),
+    "Gradient Boosting": dict(learning_rate=0.1, n_estimators=100),
     # "XGBoost":
-    # "Naive Bayes":
-    # "Perceptron":
-    # "Logistic Regression":
-    # "LDA":
-    # "SVM":
+    "Naive Bayes":dict(var_smoothing=1e-9),
+    "Perceptron":dict(alpha=0.001, penalty='l1'),
+    "Logistic Regression": dict(C=1, penalty='l2'),
+    "LDA":dict(),
+    "SVM":dict(kernel='rbf', C=1)
 }
 
 def run_method(method_name):
     # 这个函数用于调试某一个特定方法的超参数，并在训练、验证集上测试准确率
     train_results, val_results = {}, {}
-    hparams = model_hparams.get(method_name, dict())
+    hparams = model_hparams_best.get(method_name, dict())
     print(hparams)
     for name, X_train, X_val, y_train, y_val in zip(
             datasets.keys(), X_train_sets.values(), X_val_sets.values(), y_train_sets.values(), y_val_sets.values()
@@ -132,40 +126,119 @@ def report_result(runs = 5):
     plt.show()
 
 if __name__ == "__main__":
-    model = 'Decision Tree'
-    print(model)
-    model_hparams_type = ['criterion', 'max_depth']
-    param1 = ['log_loss', 'entropy', 'gini', ]
-    param2 = [3, 5, 10, 12]
-    model_hparams_values = []
-    for i in param1:
-        for j in param2:
-            my_dict = dict()
-            my_dict[model_hparams_type[0]] = i
-            my_dict[model_hparams_type[1]] = j
-            model_hparams_values.append(my_dict)
-    print('Hyperparameter Options:')
-    for item in model_hparams_values:
-        print(item)
+    datanames = ['iris', 'breast_cancer', 'usps']
     
-    vad_dat = []
-    vad_mean = []
-    for param in model_hparams_values:
-        train_results, val_results, hparams = run_hparams(model, param)
-        vad_dat.append(val_results)
+    # model = 'Decision Tree'
+    # model_hparams_type = ['max_depth', 'criterion',]
+    # param1 = [3, 5, 10, 12]
+    # param2 = ['entropy', 'gini', 'log_loss']
+    
+    # model = 'Random Forest'
+    # model_hparams_type = ['n_estimators', 'criterion',]
+    # param1 = [20, 50, 100, 200]
+    # param2 = ['entropy', 'gini', 'log_loss']
+    
+    # model = 'Bagging'
+    # model_hparams_type = ['n_estimators']
+    # param1 = [2, 5, 10, 20]
+    
+    # model = 'Gradient Boosting'
+    # model_hparams_type = ['n_estimators', 'learning_rate',]
+    # param1 = [10, 20, 50, 100, 200]
+    # param2 = [1e-3, 1e-2, 1e-1]
+    
+    # model = 'Naive Bayes'
+    # model_hparams_type = ['var_smoothing']
+    # param1 = [1e-9, 1e-7, 1e-3, 1e-1]
+    
+    # model = 'Perceptron'
+    # model_hparams_type = ['alpha', 'penalty',]
+    # param1 = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    # param2 = [None, 'l1', 'l2']
 
-    print('\nMean Validation Error')
-    for i,param in enumerate(model_hparams_values):
-        print(vad_dat[i])
-        mean = sum(vad_dat[i].values())/len(vad_dat[i])
-        print(param, mean)
-        vad_mean.append(mean)
+    # model = 'Logistic Regression'
+    # model_hparams_type = ['C', 'penalty']#, 'solver']
+    # param1 = [0.1, 1, 10]
+    # param2 = [None, 'l2']
+    # param3 = ['lbfgs', 'liblinear', 'lbfgs']
     
-    ind_best = np.argmin(vad_mean)
-    print('\nBest Hyperparams for ', model)
-    print(model_hparams_values[ind_best])
+    # model = 'LDA'
+    # vad_dat = []
+    # vad_mean = []
+    # train_results, val_results, hparams = run_method(model)
+    # vad_dat= val_results
+    # print(vad_dat)
+    # print(sum(vad_dat.values())/len(vad_dat))
+    
+    # model = 'SVM'
+    # model_hparams_type = ['C', 'kernel']
+    # param1 = [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000]
+    # param2 = ['linear', 'poly', 'rbf', 'sigmoid']
+
+    
+    # print(model)
+    # model_hparams_values = []
+    # if len(model_hparams_type)==1:
+    #     for i in param1:
+    #         my_dict = dict()
+    #         my_dict[model_hparams_type[0]] = i
+    #         model_hparams_values.append(my_dict)
+    # if len(model_hparams_type)==3:
+    #     for i in param1:
+    #         for j in param2:
+    #             for k in param3:
+    #                 my_dict = dict()
+    #                 my_dict[model_hparams_type[0]] = i
+    #                 my_dict[model_hparams_type[1]] = j
+    #                 my_dict[model_hparams_type[2]] = k
+    #                 model_hparams_values.append(my_dict)
+    # else:
+    #     for i in param1:
+    #         for j in param2:
+    #             my_dict = dict()
+    #             my_dict[model_hparams_type[0]] = i
+    #             my_dict[model_hparams_type[1]] = j
+    #             model_hparams_values.append(my_dict)
+    # print('Hyperparameter Options:')
+    # for item in model_hparams_values:
+    #     print(item)
+    
+    # vad_dat = []
+    # vad_mean = []
+    # for param in model_hparams_values:
+    #     train_results, val_results, hparams = run_hparams(model, param)
+    #     vad_dat.append(val_results)
+
+    # print('\nMean Validation Error')
+    # for i,param in enumerate(model_hparams_values):
+    #     # print(vad_dat[i])
+    #     mean = sum(vad_dat[i].values())/len(vad_dat[i])
+    #     # print(param, mean)
+    #     vad_mean.append(mean)
+   
+    # if len(model_hparams_type)==1:
+    #     for i in range(4):
+    #         row = str(param1[i])
+    #         for k in range(3):
+    #             row += ' & '
+    #             row += str(round(vad_dat[i][datanames[k]], 3))
+    #         row += ' \\\ \hline'
+    #         print(row)
+    # else:
+    #     for i in range(len(param1)):
+    #         row = str(param1[i])
+    #         for j in range(len(param2)):
+    #             for k in range(len(datanames)):
+    #                 row += ' & '
+    #                 row += str(round(vad_dat[j*3 + i][datanames[k]], 3))
+    #         row += ' \\\ \hline'
+    #         print(row)
+    
+    # ind_best = np.argmax(vad_mean)
+    # print('\nBest Hyperparams for ', model)
+    # print(model_hparams_values[ind_best])
     # train_results, val_results, hparams = run_method(model)
     # print(f'train accuracy on each dataset: {train_results}'
     #       f'\nval accuracy on each dataset: {val_results}'
     #       f'\nhparams: {hparams}')
-    # report_result()
+    report_result()
