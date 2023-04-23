b7047c1982240201fd8dd805a2e46742ce2cad29
ellahu <1228606655@qq.com>
2023-04-24 00:28:38 +0800

    Finish Task3 in hw8

--- hw8/hw8-QingruHu/semi_sup.py
+++ hw8/hw8-QingruHu/semi_sup.py
@@ -1,6 +1,11 @@
 import numpy as np
 from sklearn.datasets import fetch_openml, load_digits, load_iris
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.manifold import MDS
+from sklearn.manifold import Isomap
+from sklearn.manifold import LocallyLinearEmbedding as LLE
+from sklearn.manifold import TSNE
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import kneighbors_graph
@@ -43,6 +48,7 @@ class LapRLS:
         self.graph_weights = graph_weights
 
     def fit(self, X_l, X_u, Y_l):
+        # define Y
         l = len(X_l)
         u = len(X_u)
         X = np.concatenate([X_l, X_u])
@@ -67,7 +73,7 @@ class LapRLS:
 
     def predict(self, X):
         K1 = self.kernel(X, self.X, self.kernel_param)
-        return K1 @ self.W
+        return K1.dot(self.W)
 
 
 def load_dataset(name='digits'):
@@ -75,6 +81,7 @@ def load_dataset(name='digits'):
         dset = load_digits()
     elif name == 'usps':
         dset = fetch_openml('usps', version=1)
+        # dset = np.load('usps.arff', allow_pickle=True)
     else:
         raise ValueError("Invalid dataset name")
 
@@ -92,14 +99,8 @@ def highlight_wrong_points(ax, X_t, y, y_pred, title):
 
     cmap = plt.cm.Set1
     norm = plt.Normalize(y.min(), y.max())
-    print(correct.shape)
-    correct = correct.flatten()
-    print(correct.shape)
-    # correct = list(correct)
-    # incorrect = incorrect.T
-    # print(X_t[correct, 0])
 
-    ax.scatter(X_t[correct, 0], X_t[correct, 1])#, c=cmap(norm(y[correct])), edgecolor='k', marker='o')
+    ax.scatter(X_t[correct, 0], X_t[correct, 1], c=cmap(norm(y[correct])), edgecolor='k', marker='o')
     ax.scatter(X_t[incorrect, 0], X_t[incorrect, 1], c=cmap(norm(y[incorrect])), edgecolor='k', marker='x')
 
     ax.set_title(title)
@@ -129,6 +130,7 @@ def main(dataset_name):
     n_classes = len(np.unique(y))
     y_train_one_hot = np.eye(n_classes)[y_train_labeled]
 
+    ## RLS
     # 创建并训练模型
     rls = RLS()
     rls.fit(X_train_labeled, y_train_one_hot)
@@ -136,11 +138,18 @@ def main(dataset_name):
     # 预测
     y_pred_rls = rls.predict(X_test)
     y_pred_rls = np.argmax(y_pred_rls, axis=1).squeeze()
+    # fix the squeeze bug
+    if len(y_pred_rls.shape)>1:
+        y = []
+        for i in y_pred_rls:
+            y.append(i.item())
+        y_pred_rls = np.array(y)
 
     # 计算准确率
     accuracy_RLS = np.mean(y_pred_rls == y_test)
     print("RLS Accuracy:", accuracy_RLS)
 
+    ## LapRLS
     # 使用验证集选择最佳的gamma_I
     best_gamma_I = 0
     best_accuracy = 0
@@ -150,6 +159,14 @@ def main(dataset_name):
 
         y_pred_val = laprls.predict(X_val)
         y_pred_val = np.argmax(y_pred_val, axis=1).squeeze()
+        # fix the squeeze bug
+        if len(y_pred_val.shape)>1:
+            y_pred_val = laprls.predict(X_val)
+            y_pred_val = np.argmax(y_pred_val, axis=1)
+            y = []
+            for i in y_pred_val:
+                y.append(i.item())
+            y_pred_val = np.array(y)
         accuracy = np.mean(y_pred_val == y_val)
         # print(gamma_I, accuracy)
         if accuracy > best_accuracy:
@@ -163,19 +180,26 @@ def main(dataset_name):
     # 预测
     y_pred_laprls = laprls.predict(X_test)
     y_pred_laprls = np.argmax(y_pred_laprls, axis=1).squeeze()
+    # fix the squeeze bug
+    if len(y_pred_laprls.shape)>1:
+        y_pred_laprls = laprls.predict(X_test)
+        y_pred_laprls = np.argmax(y_pred_laprls, axis=1)
+        y = []
+        for i in y_pred_laprls:
+            y.append(i.item())
+        y_pred_laprls = np.array(y)
 
     # 计算准确率
     accuracy_manifold = np.mean(y_pred_laprls == y_test)
     print(f"LapRLS Accuracy (best gamma_I = {best_gamma_I}):", accuracy_manifold)
 
     methods = {
-        # TODO: 调用sklearn中合适的降维方法，下面给出了PCA的例子
         'PCA': PCA(n_components=2),
-        # 'LDA':
-        # 'MDS':
-        # 'Isomap':
-        # 'LLE':
-        # 't-SNE':
+        'LDA':LDA(),
+        'MDS':MDS(n_components=2),
+        'Isomap':Isomap(n_components=2),
+        'LLE':LLE(n_components=2),
+        't-SNE':TSNE(n_components=2)
     }
 
     # 绘制降维后的可视化图
@@ -193,5 +217,105 @@ def main(dataset_name):
         fig.savefig(f"{model}.pdf")
 
 
+def cal_accuracy(dataset_name):
+    # 加载数据集
+    X, y = load_dataset(dataset_name)
+
+    # 预处理
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # 划分训练、验证、测试集
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=1 / 9, random_state=42)
+
+    # 创建半监督数据集
+    n_labeled = len(X_train) // 10
+    mask = np.zeros(len(X_train), dtype=bool)
+    mask[:n_labeled] = True
+    np.random.shuffle(mask)
+
+    # 监督线性回归（仅使用带有标签的样本）
+    X_train_labeled = X_train[mask]
+    y_train_labeled = y_train[mask]
+    n_classes = len(np.unique(y))
+    y_train_one_hot = np.eye(n_classes)[y_train_labeled]
+
+    ## RLS
+    # 创建并训练模型
+    rls = RLS()
+    rls.fit(X_train_labeled, y_train_one_hot)
+
+    # 预测
+    y_pred_rls = rls.predict(X_test)
+    y_pred_rls = np.argmax(y_pred_rls, axis=1).squeeze()
+    if len(y_pred_rls.shape)>1:
+        y = []
+        for i in y_pred_rls:
+            y.append(i.item())
+        y_pred_rls = np.array(y)
+
+    # 计算准确率
+    accuracy_RLS = np.mean(y_pred_rls == y_test)
+    print("RLS Accuracy:", accuracy_RLS)
+
+    ## LapRLS
+    # 使用验证集选择最佳的gamma_I
+    # best_gamma_I = 0
+    # best_accuracy = 0
+    # for gamma_I in np.logspace(1, 5, 5):
+    #     print(gamma_I)
+    #     laprls = LapRLS(gamma_I=gamma_I)
+    #     laprls.fit(X_train[mask], X_train[~mask], y_train_one_hot)
+
+    #     y_pred_val = laprls.predict(X_val)
+    #     y_pred_val = np.argmax(y_pred_val, axis=1).squeeze()
+    #     if len(y_pred_val.shape)>1:
+    #         # fix the bug
+    #         y = []
+    #         for i in y_pred_val:
+    #             y.append(i.item())
+    #         y_pred_val = np.array(y)
+    #     accuracy = np.mean(y_pred_val == y_val)
+    #     # print(gamma_I, accuracy)
+    #     if accuracy > best_accuracy:
+    #         best_accuracy = accuracy
+    #         best_gamma_I = gamma_I
+
+    # 使用最佳的gamma_I重新训练模型
+    # laprls = LapRLS(gamma_I=best_gamma_I)
+    laprls = LapRLS(gamma_I=1e3)
+    laprls.fit(X_train[mask], X_train[~mask], y_train_one_hot)
+
+    # 预测
+    y_pred_laprls = laprls.predict(X_test)
+    y_pred_laprls = np.argmax(y_pred_laprls, axis=1).squeeze()
+    if len(y_pred_laprls.shape)>1:
+        y = []
+        for i in y_pred_laprls:
+            y.append(i.item())
+        y_pred_laprls = np.array(y)
+
+    # 计算准确率
+    accuracy_manifold = np.mean(y_pred_laprls == y_test)
+    print(f"LapRLS Accuracy (best gamma_I = {best_gamma_I}):", accuracy_manifold)
+    return accuracy_RLS, accuracy_manifold
+
+
 if __name__ == '__main__':
     main('digits')  # 更改数据集名称以尝试不同的数据集
+    # datasets = ['digits', 'usps']
+    # datasets = ['usps']
+    # accuracy_RLS_all = []
+    # accuracy_manifold_all = []
+    # for dataset in datasets:
+    #     print('#'*10, dataset, '#'*10)
+    #     for i in range(5):
+    #         accuracy_RLS, accuracy_manifold = cal_accuracy(dataset)
+    #         accuracy_RLS_all.append(accuracy_RLS)
+    #         accuracy_manifold_all.append(accuracy_manifold)
+    #     print('#'*20)
+    #     print('RLS: Mean Std')
+    #     print(np.mean(accuracy_RLS_all), np.std(accuracy_RLS_all))
+    #     print('LapRLS: Mean Std')
+    #     print(np.mean(accuracy_manifold_all), np.std(accuracy_manifold_all))
