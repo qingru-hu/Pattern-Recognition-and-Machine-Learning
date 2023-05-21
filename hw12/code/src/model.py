da3e6917683ab0f44e284ed2ae2da5a8601f6788
Ella <huqr20@mails.tsinghua.edu.cn>
2023-05-21 11:22:33 +0800

    start hw12

---
+++ hw12/code/src/model.py
@@ -0,0 +1,49 @@
+import math
+import torch
+import torch.nn as nn
+
+
+class LMModel_transformer(nn.Module):
+    # Language model is composed of three parts: a word embedding layer, a rnn network and a output layer.
+    # The word embedding layer have input as a sequence of word index (in the vocabulary) and output a sequence of vector where each one is a word embedding.
+    # The rnn network has input of each word embedding and output a hidden feature corresponding to each word embedding.
+    # The output layer has input as the hidden feature and output the probability of each word in the vocabulary.
+    def __init__(self, nvoc, dim=256, nhead=8, num_layers = 4):
+        super(LMModel_transformer, self).__init__()
+        self.drop = nn.Dropout(0.5)
+        self.encoder = nn.Embedding(nvoc, dim)
+        # WRITE CODE HERE witnin two '#' bar
+        ########################################
+        # Construct you Transformer model here. You can add additional parameters to the function.
+
+        ########################################
+
+        self.decoder = nn.Linear(dim, nvoc)
+        self.init_weights()
+
+    def init_weights(self):
+        init_uniform = 0.1
+        self.encoder.weight.data.uniform_(-init_uniform, init_uniform)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-init_uniform, init_uniform)
+
+    def forward(self, input):
+        #print(input.device)
+        embeddings = self.drop(self.encoder(input))
+
+        # WRITE CODE HERE within two '#' bar
+        ########################################
+        # With embeddings, you can get your output here.
+        # Output has the dimension of sequence_length * batch_size * number of classes
+        L = embeddings.size(0)
+        src_mask = torch.triu(torch.ones(L, L) * float('-inf'), diagonal=1).to(input.device.type)
+        src = embeddings * math.sqrt(self.dim)
+        #TODO: use your defined transformer, and use the mask.
+        
+
+
+        ########################################
+        output = self.drop(output)
+        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        return decoded.view(output.size(0), output.size(1), decoded.size(1))
+
