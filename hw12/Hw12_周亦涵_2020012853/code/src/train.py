e80153cfe252cd21196bffcde1f90a4cf6945fd4
ellahu <1228606655@qq.com>
2023-05-25 20:39:34 +0800

    finish hw12

---
+++ hw12/Hw12_周亦涵_2020012853/code/src/train.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+import argparse
+import time
+import math
+import torch
+import torch.optim as optim
+import torch.nn as nn
+
+import data
+import model
+import os
+import os.path as osp
+
+parser = argparse.ArgumentParser(description='PyTorch ptb Language Model')
+parser.add_argument('--epochs', type=int, default=25,
+                    help='upper epoch limit')
+parser.add_argument('--train_batch_size', type=int, default=20, metavar='N',
+                    help='batch size')
+parser.add_argument('--eval_batch_size', type=int, default=10, metavar='N',
+                    help='eval batch size')
+parser.add_argument('--max_sql', type=int, default=256,
+                    help='sequence length')
+parser.add_argument('--seed', type=int, default=1234,
+                    help='set random seed')
+parser.add_argument('--num_layers', type=int, default=4)
+parser.add_argument('--num_heads', type=int, default=8)
+parser.add_argument('--emb_dim', type=int, default=256)
+parser.add_argument('--use_pe', action="store_true")
+parser.add_argument('--cuda', action='store_true', help='use CUDA device')
+parser.add_argument('--gpu_id', type=int, default=0, help='GPU device id used')
+
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+
+# Use gpu or cpu to train
+use_gpu = True
+
+if use_gpu:
+    torch.cuda.set_device(args.gpu_id)
+    device = torch.device(args.gpu_id)
+else:
+    device = torch.device("cpu")
+
+# load data
+train_batch_size = args.train_batch_size
+eval_batch_size = args.eval_batch_size
+batch_size = {'train': train_batch_size,'valid':eval_batch_size}
+data_loader = data.Corpus("../data/ptb", batch_size, args.max_sql)
+
+########################################
+# Build LMModel model (bulid your language model here)
+model = model.LMModel_transformer(nvoc = len(data_loader.vocabulary), num_layers = args.num_layers,
+                      dim = args.emb_dim, nhead = args.num_heads)
+model = model.to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+criterion = nn.CrossEntropyLoss()
+
+# Evaluation Function
+# Calculate the average cross-entropy loss between the prediction and the ground truth word.
+# And then exp(average cross-entropy loss) is perplexity.
+def evaluate():
+    data_loader.set_valid()
+    data, target, end_flag = data_loader.get_batch()
+    model.eval()
+    idx = 0
+    avg_loss = 0
+    print(f"Validating")
+    while not end_flag:
+        with torch.no_grad():
+            data, target, end_flag = data_loader.get_batch()
+            data = data.to(device)
+            target = target.to(device)
+            decode = model(data)
+
+            # Calculate cross-entropy loss
+            loss = criterion(decode.view(decode.size(0) * decode.size(1), -1), target)
+            avg_loss += loss
+            idx += 1
+    print(f"The average loss is {avg_loss / idx}")
+    return math.exp(avg_loss.item() / idx)
+
+
+# Train Function
+def train():
+    data_loader.set_train()
+    data, target, end_flag = data_loader.get_batch()
+    model.train()
+    idx = 0
+    avg_loss = 0
+    while not end_flag:
+        data, target, end_flag = data_loader.get_batch()
+        data = data.to(device)
+        target = target.to(device)
+        decode = model(data)
+
+        # Calculate cross-entropy loss
+        optimizer.zero_grad()
+        loss = criterion(decode.view(decode.size(0)*decode.size(1), -1), target)
+        loss.backward()
+        optimizer.step()
+        if (idx+1) % 50 == 0:
+            print(f"The loss is {loss}")
+        idx += 1
+        avg_loss += loss
+    return math.exp(avg_loss.item() / idx)
+
+
+# Loop over epochs.
+train_perplexity = []
+valid_perplexity = []
+for epoch in range(1, args.epochs+1):
+    print(f"Start training epoch {epoch}")
+    train_perplexity.append(train())
+    valid_perplexity.append(evaluate())
+
+print(f"Train Perpelexity {train_perplexity}")
+print(f"Valid Perpelexity {valid_perplexity}")
+
+from matplotlib import pyplot as plt
+plt.figure(figsize=(10,10))
+plt.plot(train_perplexity,color='r',label="Train Perpelexity")
+plt.plot(valid_perplexity,color='b',label="Valid Perpelexity")
+plt.title('training and validation curves')
+plt.xlabel("Epoch")
+plt.ylabel("Perplexity")
+plt.legend()
+plt.savefig('./outcome.png')
+plt.show()
